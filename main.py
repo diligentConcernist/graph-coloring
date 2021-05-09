01208d79f366e36eef0edd95cddeb5b36758ca43
Diana Gruzdeva <taciturn.homebody@gmail.com>
2021-05-09 14:27:01 +0300

    add input graph as petersen graph

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ def CreateGraph():
 	n = int(f.readline())
 	for i in range(n):
 		graph_edge_list = f.readline().split()
-		G.add_edge(graph_edge_list[0], graph_edge_list[1]) 
+		G.add_edge(int(graph_edge_list[0]), int(graph_edge_list[1])) 
 	return G
 
 if __name__ == "__main__":
   G = CreateGraph()
-  
-  nx.draw_shell(G)
+  nx.draw_shell(G, nlist = [range(1,6), range(6,11)], with_labels = True)
   plt.show()
